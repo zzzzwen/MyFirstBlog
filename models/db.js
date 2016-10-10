122ccd88164d6b775e0719f837748dff0802d265
zzzzwen <zhiwen.song.loveslife@gmail.com>
2016-10-10 18:45:29 +0800

    V0.0

---
+++ models/db.js
@@ -0,0 +1,9 @@
+/**
+ * Created by admin on 2016/10/8 0008.
+ */
+var settings = require('../settings'),
+    Db = require('mongodb').Db,
+    Connection = require('mongodb').Connection,
+    Server = require('mongodb').Server;
+module.exports = new Db(settings.db, new Server(settings.host, settings.port),
+    {safe: true});
