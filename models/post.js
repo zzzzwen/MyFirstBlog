4d5ccca3bd3e27b90c470f7956b8de7d7d6a77e2
zzzzwen <zhiwen.song.loveslife@gmail.com>
2016-10-14 19:44:31 +0800

    Add icon

--- models/post.js
+++ models/post.js
@@ -2,8 +2,9 @@ var mongodb = require('./db'),
     Comment = require('../models/comment.js');
 
     markdown = require('markdown').markdown;
-function Post(name, title, tags, post) {
+function Post(name, head, title, tags, post) {
     this.name = name;
+    this.head = head;
     this.title = title;
     this.tags = tags;
     this.post = post;
@@ -26,6 +27,7 @@ Post.prototype.save = function(callback) {
     //要存入数据库的文档
     var post = {
         name: this.name,
+        head: this.head,
         time: time,
         title:this.title,
         tags: this.tags,
